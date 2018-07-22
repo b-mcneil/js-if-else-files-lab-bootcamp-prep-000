9563f31b38e7ac76f7f424975ef790b825a19cb6
Bri McNeil <mcneilbri@gmail.com>
2018-07-22 23:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct.")
+} else {
+  console.log("The expected name is Susan")
 }
   
 // Don't worry about this
